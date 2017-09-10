b5718eb134e41c58282335b8e47e9e599c982823
Ramesh <ramesh7214@gmail.com>
2017-09-10 15:40:15 +0530

    First Commit of Veggie

---
+++ configuration/README.txt
@@ -0,0 +1,7 @@
+#nothing to read
+
+#Maven help
+
+mvn dependency::tree - to check missed dependencies
+mvn clean install -U - it clean and rebuild the aplication and download the dependencies
+mvn clean install -DskipTests - to skip test cases (not recommended)
---
+++ services/veggieland-services/src/main/java/com/veggieland/services/VeggielandServicesApplication.java
@@ -0,0 +1,12 @@
+package com.veggieland.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggielandServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggielandServicesApplication.class, args);
+	}
+}
